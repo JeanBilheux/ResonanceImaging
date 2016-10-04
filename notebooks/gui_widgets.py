d62b7d61d7599e00bef5f1089bf3569784b1074c
JeanBilheux <bilheuxjm@ornl.gov>
2016-10-04 13:36:01 -0400

    Implemented notebook that load the resonance ascii table

---
+++ notebooks/gui_widgets.py
@@ -0,0 +1,43 @@
+from PyQt4 import QtCore, QtGui
+import os
+from decorators import format_directory
+
+
+@format_directory
+def gui_dname(dir=None):
+    """Select files"""
+    dirname = QtGui.QFileDialog.getExistingDirectory(None, "Select Folder ...", 
+                                            dir, 
+                                            QtGui.QFileDialog.ShowDirsOnly)
+    return dirname
+
+
+@format_directory
+def gui_fname(dir=None):
+    """Select one or more file via a dialog and returns the file name.
+    """
+    fname = QtGui.QFileDialog.getOpenFileNames(None, "Select file(s)...", 
+            directory = dir, filter="Fits files(*.fits);;TIFF files(*.tif)")
+    return fname
+
+
+@format_directory
+def gui_single_file(dir=None):
+    """Select one o file via a dialog and returns the file name.
+    """
+    if dir is None: 
+        dir ='./'
+    fname = QtGui.QFileDialog.getOpenFileName(None, "Select file...", 
+            dir, filter="Spectra File (*_Spectra.txt)")
+    return fname
+
+
+@format_directory
+def gui_csv_fname(dir=None):
+    """Select one or more file via a dialog and returns the file name.
+    """
+    if dir is None: 
+        dir ='./'
+    fname = QtGui.QFileDialog.getOpenFileName(None, "Select file(s)...", 
+            dir, filter="Text files (*.txt);;ASCII files(*.csv)")
+    return fname
---
+++ notebooks/load_handler.py
@@ -0,0 +1,78 @@
+import os
+import pyfits
+from PIL import Image
+import numpy as np
+
+
+def load_data(file_name):
+    '''
+    load the various file_name format
+    '''
+    data_type = get_data_type(file_name)
+    if data_type == '.fits':
+        hdulist = pyfits.open(file_name)
+        hdu = hdulist[0]
+        _image = np.asarray(hdu.data)
+        return _image
+    
+    elif (data_type == '.tif') or (data_type == '.tiff'):
+        _image = Image.open(file_name)
+        _image = np.asarray(_image)
+        return _image
+    
+    else:
+        raise NotImplementedError
+    
+    
+def get_data_type(file_name):
+    '''
+    using the file name extension, will return the type of the data
+    
+    Arguments:
+        full file name
+        
+    Returns:
+        file extension    ex:.tif, .fits
+    '''
+    filename, file_extension = os.path.splitext(file_name)
+    return file_extension.strip()
+
+def load_csv_table_file(file_name):
+    '''
+    load a csv file
+
+    ex:
+    # metadata 1
+    # metadata 2
+    var1, val1, val2, val3
+    var2, val3, val4, val5
+
+    will return a dictionary of the metadata and values
+
+    {'metadata': ['metadata1','metadata2'],
+    'data': {'var1': ['val1','val2','val3'],
+             'var2': ['val3','val4','val5']}
+             }
+    '''
+    if not os.path.isfile:
+        raise IOError ("File does not exist!")
+
+    _metadata = []
+    _data = {}
+    with open(file_name) as f:
+        for line in f:
+            li = line.strip()
+            if li.startswith('#'):
+                _metadata.append(li)
+            else:
+                _data_line = li.split(',')
+                if len(_data_line)>1:
+                    _data_float = [flaot(value) for value in _data_line[1:]]
+                    _data[_data_line[0]] = _data_float
+
+    result = {'metadata': _metadata,
+              'data': _data}
+
+    return result
+
+    
